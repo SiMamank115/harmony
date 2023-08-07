88157d759986b9a167d2b92cfb9e4c32054632bf
SiMamank115 <faeest.115@gmail.com>
2023-08-07 10:11:36 +0700

    recreated popup | 50%

--- src/utils/compactUtils.js
+++ src/utils/compactUtils.js
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+
 export function compactUtils() {
-    String.prototype.uppercaseFirst = function () {
-        return this.charAt(0).toUpperCase() + this.slice(1);
-    };
-    String.prototype.checkRoute = function (string = "", index = 0) {
-        return string == "/" ? this == string : this.includes(string, index);
-    };
+	String.prototype.uppercaseFirst = function () {
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	};
+	String.prototype.checkRoute = function (string = "", index = 0) {
+		return string == "/" ? this == string : this.includes(string, index);
+	};
+	useEffect(() => {
+		if (!localStorage?.theme) {
+			if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+				localStorage.theme = "dark";
+			} else {
+				localStorage.theme = "light";
+			}
+		}
+	}, []);
 }
